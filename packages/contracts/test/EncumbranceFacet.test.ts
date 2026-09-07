6f2068066e2af0fa04543211a8bb50943530811a
Rustacle <abrahamrustydj@gmail.com>
2026-09-07 11:54:17 +0700

    Remove deprecated EncumbranceFacet

--- packages/contracts/test/EncumbranceFacet.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe('Hypotheca EncumbranceFacet', function () {
-  it('should scaffold the contract test suite', function () {
-    expect(true).to.equal(true);
-  });
-});
