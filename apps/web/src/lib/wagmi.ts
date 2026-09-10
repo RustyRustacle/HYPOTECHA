e82852755feec8e2697cef28108f6e55318cb97e
Rustacle <abrahamrustydj@gmail.com>
2026-09-11 01:40:50 +0700

    Add wagmi config with Hedera testnet chain

---
+++ apps/web/src/lib/wagmi.ts
@@ -0,0 +1,30 @@
+import { defineChain } from 'viem'
+import { createConfig, http } from 'wagmi'
+import { injected, metaMask } from 'wagmi/connectors'
+
+export const hederaTestnet = defineChain({
+  id: 296,
+  name: 'Hedera Testnet',
+  nativeCurrency: { name: 'HBAR', symbol: 'HBAR', decimals: 8 },
+  rpcUrls: {
+    default: { http: ['https://testnet.hashio.io/api'] },
+  },
+  blockExplorers: {
+    default: { name: 'HashScan', url: 'https://hashscan.io/testnet' },
+  },
+  testnet: true,
+})
+
+export const config = createConfig({
+  chains: [hederaTestnet],
+  connectors: [metaMask(), injected()],
+  transports: {
+    [hederaTestnet.id]: http('https://testnet.hashio.io/api'),
+  },
+})
+
+declare module 'wagmi' {
+  interface Register {
+    config: typeof config
+  }
+}
