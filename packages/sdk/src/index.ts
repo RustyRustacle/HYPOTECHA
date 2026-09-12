36adf684fc780b7958136dd89dac26e010156133
Rustacle <abrahamrustydj@gmail.com>
2026-09-13 05:11:53 +0700

    Remove unused API registry and SDK services

--- packages/sdk/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { AnchorClient } from './anchor.js';
-import { AtsToken } from './ats.js';
-import { HcsBus } from './hcs.js';
-import { buildEnvelope, checkEnoughAvailable } from './enforcement.js';
-
-export * from './types.js';
-export { AnchorClient, AtsToken, HcsBus, checkEnoughAvailable, buildEnvelope };
--- services/registry/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { RegistryProjection, type HoldRecord, type HolderSummary } from './projection.js';
-export { RegistrySync, type RegistrySyncConfig } from './sync.js';
-export { makeVerdict, type GuardVerdict } from './guard.js';
